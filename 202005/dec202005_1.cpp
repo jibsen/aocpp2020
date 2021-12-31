a6d175b9c0cf83fc33fcf97ddfb731e664b7577c
Joergen Ibsen <ji@ibse.dk>
2021-12-31 09:01:58 +0100

    Day 5 part one

---
+++ 202005/dec202005_1.cpp
@@ -0,0 +1,36 @@
+//
+// Advent of Code 2020, day 5, part one
+//
+
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <vector>
+
+constexpr unsigned int seat_to_id(std::string_view seat)
+{
+	unsigned int id = 0;
+	unsigned int mask = 1;
+
+	for (auto ch : seat | std::views::reverse) {
+		if (ch == 'B' || ch == 'R') {
+			id |= mask;
+		}
+		mask <<= 1;
+	}
+
+	return id;
+}
+
+int main()
+{
+	unsigned int max_value = 0;
+
+	for (std::string line; std::getline(std::cin, line); ) {
+		max_value = std::max(max_value, seat_to_id(line));
+	}
+
+	std::cout << max_value << '\n';
+}
