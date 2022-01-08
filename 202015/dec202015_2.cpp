a7d68ecdf2a3ea68e41ddc790d02eafa6ec27adf
Joergen Ibsen <ji@ibse.dk>
2022-01-08 10:18:56 +0100

    Day 15 part two

---
+++ 202015/dec202015_2.cpp
@@ -0,0 +1,48 @@
+//
+// Advent of Code 2020, day 15, part two
+//
+
+// There might be a repeating pattern in the output we could look for, but
+// the number of iterations is low enough we can simply run them.
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+auto read_starting_numbers()
+{
+	std::vector<long long> numbers;
+
+	long long num = 0;
+
+	for (char sep = ','; sep == ',' && std::cin >> num; std::cin >> sep) {
+		numbers.push_back(num);
+	}
+
+	return numbers;
+}
+
+int main()
+{
+	auto numbers = read_starting_numbers();
+
+	std::unordered_map<long long, long long> last_seen;
+
+	for (int turn = 1; turn < numbers.size(); ++turn) {
+		last_seen.insert({numbers[turn - 1], turn});
+	}
+
+	long long next = numbers.back();
+
+	for (long long turn = numbers.size(); turn < 30000000; ++turn) {
+		if (auto [it, success] = last_seen.insert({next, turn}); success) {
+			next = 0;
+		}
+		else {
+			next = turn - it->second;
+			it->second = turn;
+		}
+	}
+
+	std::cout << next << '\n';
+}
