cfa6a6c1b75f9632a7c179478cd4597a3ed02206
Joergen Ibsen <ji@ibse.dk>
2022-01-13 07:30:56 +0100

    Day 11 part one

---
+++ 202011/dec202011_1.cpp
@@ -0,0 +1,90 @@
+//
+// Advent of Code 2020, day 11, part one
+//
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <ranges>
+#include <string>
+#include <vector>
+
+using Map = std::vector<std::string>;
+
+auto read_map()
+{
+	Map map;
+
+	for (std::string line; std::cin >> line; ) {
+		map.push_back(std::move(line));
+	}
+
+	return map;
+}
+
+std::size_t num_neighbors(const Map &map, int x, int y)
+{
+	std::array<std::pair<int, int>, 8> directions = {{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1}
+	}};
+
+	auto inside_map = [&map](int x, int y) {
+		return y >= 0 && y < map.size()
+		    && x >= 0 && x < map[y].size();
+	};
+
+	return std::ranges::count_if(directions,
+		[&](auto dir) {
+			auto [dx, dy] = dir;
+
+			return inside_map(x + dx, y + dy)
+			    && map[y + dy][x + dx] == '#';
+		}
+	);
+}
+
+bool update_map(Map &map)
+{
+	Map new_map(map);
+
+	bool change = false;
+
+	for (int y = 0; y < map.size(); ++y) {
+		for (int x = 0; x < map[y].size(); ++x) {
+			switch (map[y][x]) {
+			case 'L':
+				if (num_neighbors(map, x, y) == 0) {
+					new_map[y][x] = '#';
+					change = true;
+				}
+				break;
+			case '#':
+				if (num_neighbors(map, x, y) >= 4) {
+					new_map[y][x] = 'L';
+					change = true;
+				}
+				break;
+			}
+		}
+	}
+
+	map.swap(new_map);
+
+	return change;
+}
+
+int main()
+{
+	auto map = read_map();
+
+	for (;;) {
+		if (!update_map(map)) {
+			break;
+		}
+	}
+
+	std::cout << std::ranges::count(map | std::views::join, '#') << '\n';
+}
